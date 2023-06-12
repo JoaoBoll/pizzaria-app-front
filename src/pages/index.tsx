2a754495a1c4c83de189a5cfa97020ccfc1da193
João Boll <joaovitorpianista@hotmail.com>
2023-06-12 10:04:24 -0300

    Criado component input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import styles from '../../styles/Home.module.scss'
+import Head from 'next/head'
+import styles from '../../styles/home.module.scss'
+import Image from 'next/image'
+import logoImg from '../../public/logo.svg'
+import { Input } from '../components/ui/Input/input'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Teste</h1>
-    </div>
+    <>
+      <Head>
+        <title>
+          Sujeito Pizza
+        </title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt = "Logo Sujeito Pizzaria"/>
+        <div className={styles.login}>
+          <form>
+            <Input placeholder='Digite seu E-mail'
+            type='text'/>
+
+            <Input placeholder='Digite sua Senha'
+            type='password'/>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
