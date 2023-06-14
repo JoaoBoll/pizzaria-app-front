4ce34156337f4372919019f81b30413dc47bf351
João Boll <joaovitorpianista@hotmail.com>
2023-06-14 13:45:21 -0300

    Criado Header superior da tela

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import Router from 'next/router'; 
+ import Router from 'next/router';
 import {createContext, ReactNode, useState} from 'react';
 import { destroyCookie, setCookie, parseCookies } from 'nookies'
 import { api } from '../services/apiClient';
